b0d200934647a367c4e075c25ceb45a7b15c9d42
tripott <tripott@gmail.com>
2017-11-15 14:58:13 -0500

    combining reducers

--- app/src/store.js
+++ app/src/store.js
@@ -1,16 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { drawer } from './reducers/drawer'
 import thunk from 'redux-thunk'
 
-export default createStore(
-  combineReducers({
-    drawer: (state = { open: true }, action) => {
-      switch (action.type) {
-        case 'TOGGLE_DRAWER':
-          return { open: !state.open }
-        default:
-          return state
-      }
-    }
-  }),
-  applyMiddleware(thunk)
-)
+export default createStore(combineReducers({ drawer }), applyMiddleware(thunk))
