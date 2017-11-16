c75a4d3f85f37f93c0136b04e67faf698ae780b7
CamIAm <rcmontgo@gmail.com>
2017-11-16 15:51:08 -0500

    legal card centered

--- app/src/pages/legal.js
+++ app/src/pages/legal.js
@@ -17,8 +17,11 @@ import Typography from "material-ui/Typography";
 import Gavel from "material-ui-icons/Gavel";
 const styles = {
   card: {
-    alignItems: "center",
-    maxWidth: 345
+    align: "center",
+    maxWidth: 345,
+    flex: 1,
+    width: "100%",
+    position: "relative"
   },
   media: {
     height: 200
@@ -30,49 +33,51 @@ function SimpleMediaCard(props) {
   return (
     <div>
       <MenuAppBar title="Legal" />
-      <Card className={classes.card}>
-        <CardHeader
-          avatar={
-            <Avatar>
-              <Gavel />
-            </Avatar>
-          }
-          title="Legal"
-          subheader="Disclaimer"
-        />
-        <CardContent>
-          <Typography component="p">
-            The organizations and individuals listed in this directory cannot
-            and do not guarantee that they can provide assistance, but each of
-            us are “committed” to doing everything within our charters and
-            organizations, bylaws, governing Federal-State-Local laws and
-            rules-regulations, and resolve to do whatever we can that is legal,
-            moral, and ethical and within in our resources and approvals
-            processes to help those in need. Everyone and every organization in
-            this directory is devoted to trying to help our beloved wounded,
-            ill, injured, and needy troops and disabled veterans, veterans, and
-            their families.Although most of the services in this directory are
-            free,some are not free.For example (and just one example) there are
-            some co-pays for medications under hospice for Medicare covered
-            hospice patients.However, all the organizations and individuals
-            listed here are committed to help in every way possible and to make
-            sure if there are any fees associated with their assistance that
-            they are fully disclosed up front and before there are any charges
-            accrued.New organizations that wish to become a member of the
-            Tri-County Veteran Support Network may submit contact information,
-            mission statement, and website address (if applicable) via e-mailto
-            tim@tcvsn.org.
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button dense color="primary">
-            About
-          </Button>
-          <Button dense color="primary">
-            In Memory
-          </Button>
-        </CardActions>
-      </Card>
+      <div style={{ width: "95%", margin: "0 auto" }}>
+        <Card className={classes.card}>
+          <CardHeader
+            avatar={
+              <Avatar>
+                <Gavel />
+              </Avatar>
+            }
+            title="Legal"
+            subheader="Disclaimer"
+          />
+          <CardContent>
+            <Typography component="p">
+              The organizations and individuals listed in this directory cannot
+              and do not guarantee that they can provide assistance, but each of
+              us are “committed” to doing everything within our charters and
+              organizations, bylaws, governing Federal-State-Local laws and
+              rules-regulations, and resolve to do whatever we can that is
+              legal, moral, and ethical and within in our resources and
+              approvals processes to help those in need. Everyone and every
+              organization in this directory is devoted to trying to help our
+              beloved wounded, ill, injured, and needy troops and disabled
+              veterans, veterans, and their families.Although most of the
+              services in this directory are free,some are not free.For example
+              (and just one example) there are some co-pays for medications
+              under hospice for Medicare covered hospice patients.However, all
+              the organizations and individuals listed here are committed to
+              help in every way possible and to make sure if there are any fees
+              associated with their assistance that they are fully disclosed up
+              front and before there are any charges accrued.New organizations
+              that wish to become a member of the Tri-County Veteran Support
+              Network may submit contact information, mission statement, and
+              website address (if applicable) via e-mailto tim@tcvsn.org.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button dense color="primary">
+              About
+            </Button>
+            <Button dense color="primary">
+              In Memory
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
     </div>
   );
 }
