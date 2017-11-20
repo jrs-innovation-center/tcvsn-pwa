77f5867e164f2708d1e61f16bea9e8f968696238
CamIAm <rcmontgo@gmail.com>
2017-11-20 14:54:30 -0500

    few more changes

--- app/src/components/menuAppBar.js
+++ app/src/components/menuAppBar.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { withStyles } from 'material-ui/styles'
-import AppBar from 'material-ui/AppBar'
-import Toolbar from 'material-ui/Toolbar'
-import Typography from 'material-ui/Typography'
-import IconButton from 'material-ui/IconButton'
-import MenuIcon from 'material-ui-icons/Menu'
-import SearchIcon from 'material-ui-icons/Search'
-import { connect } from 'react-redux'
+import React from "react";
+import { withStyles } from "material-ui/styles";
+import AppBar from "material-ui/AppBar";
+import Toolbar from "material-ui/Toolbar";
+import Typography from "material-ui/Typography";
+import IconButton from "material-ui/IconButton";
+import MenuIcon from "material-ui-icons/Menu";
+import SearchIcon from "material-ui-icons/Search";
+import { connect } from "react-redux";
 
 const styles = theme => ({
   root: {
     marginTop: theme.spacing.unit * 0,
-    width: '100%'
+    width: "100%"
   },
   flex: {
     flex: 1
@@ -20,11 +20,11 @@ const styles = theme => ({
     marginLeft: -12,
     marginRight: 20
   }
-})
+});
 
 //  <Button color="contrast">Search</Button>
 const MenuAppBar = props => {
-  const { classes } = props
+  const { classes } = props;
 
   return (
     <div className={classes.root}>
@@ -51,14 +51,14 @@ const MenuAppBar = props => {
         </Toolbar>
       </AppBar>
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({});
 const mapActionsToProps = dispatch => ({
-  toggleDrawer: () => dispatch({ type: 'TOGGLE_DRAWER' })
-})
+  toggleDrawer: () => dispatch({ type: "TOGGLE_DRAWER" })
+});
 
-const connector = connect(mapStateToProps, mapActionsToProps)
+const connector = connect(mapStateToProps, mapActionsToProps);
 
-export default connector(withStyles(styles)(MenuAppBar))
+export default connector(withStyles(styles)(MenuAppBar));
