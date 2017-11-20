1171ec978c2988a43335e374953800d759420e61
CamIAm <rcmontgo@gmail.com>
2017-11-20 14:47:17 -0500

    Appmenu added

--- app/src/pages/categories/show.js
+++ app/src/pages/categories/show.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { withStyles } from "material-ui/styles";
 import classnames from "classnames";
 import Card, {
+  ResourceCard,
   CardHeader,
   CardMedia,
   CardContent,
@@ -12,14 +13,17 @@ import Collapse from "material-ui/transitions/Collapse";
 import Avatar from "material-ui/Avatar";
 import Icon from "material-ui/Icon";
 import IconButton from "material-ui/IconButton";
+import Button from "material-ui/Button";
+import InboxIcon from "material-ui-icons/Inbox";
 import Typography from "material-ui/Typography";
 import red from "material-ui/colors/red";
 import FavoriteIcon from "material-ui-icons/Favorite";
 import ShareIcon from "material-ui-icons/Share";
-//import Chevron from " material-ui-icons/Chevron_left";
+//import Chevron_left from " material-ui-icons/Chevron_left";
 import ExpandMoreIcon from "material-ui-icons/ExpandMore";
 import withRoot from "../../components/withRoot";
 import withDrawer from "../../components/withDrawer";
+import MenuAppBar from "../../components/menuAppBar";
 import { connect } from "react-redux";
 import { setCurrentCategory } from "../../action-creators/categories";
 import { prop, path, split, compose, last } from "ramda";
@@ -50,12 +54,14 @@ const styles = theme => ({
 
 class ShowCategory extends React.Component {
   state = { expanded: false };
+
   componentDidMount() {
     const pathID = prop("match")(this.props)
       ? path(["match", "params", "id"])(this.props)
       : compose(last, split("/"), path(["location", "pathname"]))(this.props);
     this.props.getCategory(pathID);
   }
+
   handleExpandClick = () => {
     this.setState({ expanded: !this.state.expanded });
   };
@@ -65,6 +71,7 @@ class ShowCategory extends React.Component {
     console.log("ShowCategory props category:", category);
     return (
       <div>
+        <MenuAppBar title="Resource" search={true} goBack={true} />
         <Card>
           <CardHeader
             avatar={
@@ -82,61 +89,13 @@ class ShowCategory extends React.Component {
           <CardContent>
             <Typography component="p">{`${category.desc}`}</Typography>
           </CardContent>
+
           <CardActions disableActionSpacing>
-            <IconButton aria-label="Add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="Share">
-              <ShareIcon />
-            </IconButton>
-            <div />
-            <IconButton
-              onClick={this.handleExpandClick}
-              aria-expanded={this.state.expanded}
-              aria-label="Show more"
-            >
-              <ExpandMoreIcon />
-            </IconButton>
+            <Button raised>
+              Resources
+              <InboxIcon />
+            </Button>
           </CardActions>
-          <Collapse
-            in={this.state.expanded}
-            transitionDuration="auto"
-            unmountOnExit
-          >
-            <CardContent>
-              <Typography paragraph type="body2">
-                Method:
-              </Typography>
-              <Typography paragraph>
-                Heat 1/2 cup of the broth in a pot until simmering, add saffron
-                and set aside for 10 minutes.
-              </Typography>
-              <Typography paragraph>
-                Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                skillet over medium-high heat. Add chicken, shrimp and chorizo,
-                and cook, stirring occasionally until lightly browned, 6 to 8
-                minutes. Transfer shrimp to a large plate and set aside, leaving
-                chicken and chorizo in the pan. Add pimentón, bay leaves,
-                garlic, tomatoes, onion, salt and pepper, and cook, stirring
-                often until thickened and fragrant, about 10 minutes. Add
-                saffron broth and remaining 4 1/2 cups chicken broth; bring to a
-                boil.
-              </Typography>
-              <Typography paragraph>
-                Add rice and stir very gently to distribute. Top with artichokes
-                and peppers, and cook without stirring, until most of the liquid
-                is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
-                reserved shrimp and mussels, tucking them down into the rice,
-                and cook again without stirring, until mussels have opened and
-                rice is just tender, 5 to 7 minutes more. (Discard any mussels
-                that don’t open.)
-              </Typography>
-              <Typography>
-                Set aside off of the heat to let rest for 10 minutes, and then
-                serve.
-              </Typography>
-            </CardContent>
-          </Collapse>
         </Card>
       </div>
     );
