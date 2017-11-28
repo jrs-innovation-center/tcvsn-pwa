427bc1cf4087d4bc5783b8b4e33375ecaba052af
tripott <tripott@gmail.com>
2017-11-28 10:03:59 -0500

    starting secondary menu component

---
+++ app/src/components/secondaryMenu.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import IconButton from 'material-ui/IconButton';
+import Menu, { MenuItem } from 'material-ui/Menu';
+import MoreVertIcon from 'material-ui-icons/MoreVert';
+
+
+
+/*
+props.actions = [
+{name: 'Edit', link: '/resources/???/edit'},
+{name: 'delete, link: '/resources/???/delete'}
+]
+
+*/
+const secondaryMenu = function (props) {
+
+  return (
+
+
+  )
+
+}
