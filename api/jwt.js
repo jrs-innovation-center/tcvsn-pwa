775561ba0157e7b0955209506f1bbf0d5d10da7e
Tom Wilson <tom@jackhq.com>
2017-11-30 17:18:39 -0500

    added jwt module for api

---
+++ api/jwt.js
@@ -0,0 +1,15 @@
+// @format
+const jwt = require('express-jwt')
+const jwks = require('jwks-rsa')
+
+module.exports = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://vet-support.auth0.com/.well-known/jwks.json',
+  }),
+  audience: 'https',
+  issuer: 'https://vet-support.auth0.com/',
+  algorithms: ['RS256'],
+})
