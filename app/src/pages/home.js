b6c12420eb442224a3eac5dda73dd1851e2b2c02
Tom Wilson <tom@jackhq.com>
2018-02-24 08:14:08 -0500

    added favicon and welcome page

--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -1,29 +1,59 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 //import withRoot from '../components/withRoot'
 import withDrawer from '../components/withDrawer'
 import MenuAppBar from '../components/menuAppBar'
 import { connect } from 'react-redux'
 import { filter, contains, map } from 'ramda'
+import Typography from 'material-ui/Typography'
 import List from 'material-ui/List'
+import Button from 'material-ui/Button'
 import ResourceItem from '../components/resource-item'
+import Container from 'react-declarative-container'
+import { cond, always, T, isEmpty } from 'ramda'
 
-class Home extends React.Component {
-  componentDidMount() {
-    this.props.toggleDrawer()
-  }
-  render() {
-    return (
+const ListFavorites = ({ favorites }) => (
+  <List style={{ padding: 0, marginBottom: 60 }}>
+    {map(favorite => <ResourceItem resource={favorite} />, favorites)}
+  </List>
+)
+
+const Welcome = () => (
+  <div style={{ padding: 48 }}>
+    <center>
+      <img src="/tcvsn-ico.png" />
+      <Typography variant="display4">
+        Welcome to Veteran Support Resource App
+      </Typography>
       <div>
-        <MenuAppBar title="Home" />
-        <List style={{ padding: 0, marginBottom: 60 }}>
-          {map(
-            favorite => <ResourceItem resource={favorite} />,
-            this.props.favorites
-          )}
-        </List>
+        <Link to="/categories">
+          <Button variant="raised" color="primary">
+            Categories
+          </Button>
+        </Link>
       </div>
-    )
-  }
+    </center>
+  </div>
+)
+
+const Home = props => {
+  return (
+    <Container
+      didMount={() => {
+        props.toggleDrawer()
+      }}
+    >
+      {() => (
+        <div>
+          <MenuAppBar title="Home" />
+          {cond([
+            [isEmpty, always(<Welcome />)],
+            [T, always(<ListFavorites favorites={props.favorites} />)]
+          ])(props.favorites)}
+        </div>
+      )}
+    </Container>
+  )
 }
 
 const connector = connect(
--- docs/poi.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  presets: [require('poi-preset-react')()]
-}
